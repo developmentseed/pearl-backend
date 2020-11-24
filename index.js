063257e9bd6687b0f0275291a02bac33ee7fddb4
ingalls <nick@ingalls.ca>
2020-11-24 06:52:18 -0700

    Server frameworking

---
+++ index.js
@@ -0,0 +1,15 @@
+const ws = require('ws');
+const express = require('express');
+
+if (require.main === module) {
+    return server();
+}
+
+/**
+ * @param {Object} args
+ * @param {Config} config
+ * @param {function} cb
+ */
+async function server(args, config, cb) {
+
+}
