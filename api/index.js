5eb96f934676bc50dcac3140940f0e8be6cc6e19
ingalls <nick@ingalls.ca>
2021-01-04 08:25:36 -0700

    API Frameworking

--- api/index.js
+++ api/index.js
@@ -1,20 +1,143 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const morgan = require('morgan');
+const minify = require('express-minify');
+const bodyparser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+const pkg = require('./package.json');
+
 const argv = require('minimist')(process.argv, {
-    boolean: ['dev']
+    boolean: ['prod'],
+    string: ['postgres']
 });
+
+const pgSession = require('connect-pg-simple')(session);
+const router = express.Router();
+const app = express();
+const { Pool } = require('pg');
+const Config = require('./lib/config');
 
 const PORT = 2000;
 
 if (require.main === module) {
-    return server();
+    configure(argv);
+}
+
+function configure(argv, cb) {
+    Config.env(argv).then((config) => {
+        return server(argv, config, cb);
+    }).catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
 }
 
 /**
- * @param {Object} args
+ * @param {Object} argv
  * @param {Config} config
  * @param {function} cb
  */
-async function server(args, config, cb) {
+async function server(argv, config, cb) {
+    const pool = new Pool({
+        connectionString: config.postgres
+    });
 
-    console.error(`ok - running http://localhost:${PORT}`);
+    try {
+        await pool.query(String(fs.readFileSync(path.resolve(__dirname, 'schema.sql'))));
+    } catch (err) {
+        throw new Error(err);
+    }
+
+    app.disable('x-powered-by');
+    app.use(minify());
+    app.use(session({
+        name: argv.prod ? '__Host-session' : 'session',
+        proxy: argv.prod,
+        resave: false,
+        store: new pgSession({
+            pool: pool,
+            tableName : 'session'
+        }),
+        cookie: {
+            maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
+            sameSite: true,
+            secure: argv.prod
+        },
+        saveUninitialized: true,
+        secret: config.CookieSecret
+    }));
+
+    /**
+     * @api {get} /api Get Metadata
+     * @apiVersion 1.0.0
+     * @apiName Meta
+     * @apiGroup Server
+     * @apiPermission public
+     *
+     * @apiDescription
+     *     Return basic metadata about server configuration
+     *
+     * @apiSuccessExample Success-Response:
+     *   HTTP/1.1 200 OK
+     *   {
+     *       "version": "1.0.0"
+     *   }
+     */
+    app.get('/api', (req, res) => {
+        return res.json({
+            version: pkg.version
+        });
+    });
+
+    /**
+     * @api {get} /health Server Healthcheck
+     * @apiVersion 1.0.0
+     * @apiName Health
+     * @apiGroup Server
+     * @apiPermission public
+     *
+     * @apiDescription
+     *     AWS ELB Healthcheck for the server
+     *
+     * @apiSuccessExample Success-Response:
+     *   HTTP/1.1 200 OK
+     *   {
+     *       "healthy": true,
+     *       "message": "Good to go"
+     *   }
+     */
+    app.get('/health', (req, res) => {
+        return res.json({
+            healthy: true,
+            message: 'Good to go'
+        });
+    });
+
+    app.use('/api', router);
+
+    router.use(bodyparser.urlencoded({ extended: true }));
+    router.use(morgan('combined'));
+    router.use(bodyparser.json({
+        limit: '50mb'
+    }));
+
+    router.all('*', (req, res) => {
+        return res.status(404).json({
+            status: 404,
+            message: 'API endpoint does not exist!'
+        });
+    });
+
+    const srv = app.listen(PORT, (err) => {
+        if (err) return err;
+
+        if (cb) return cb(srv, pool);
+
+        console.error(`ok - running http://localhost:${PORT}`);
+    });
+
 }
