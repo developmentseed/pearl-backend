2d0b60b1f4b96da500b481d15b3facbb8d449e23
Vitor George <vitor.george@gmail.com>
2021-02-02 18:56:00 +0000

    Do not require Auth0 config (not used in tests)

--- services/api/lib/config.js
+++ services/api/lib/config.js
@@ -17,12 +17,9 @@ class Config {
 
         this.Port = args.port || 2000;
 
-        if (!process.env.AUTH0_ISSUER_BASE_URL || !process.env.AUTH0_CLIENT_ID) {
-            throw new Error('AUTH0_ISSUER_BASE_URL and AUTH0_CLIENT_ID must be set.');
-        }
-        this.Auth0IssuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
-        this.Auth0ClientId = process.env.AUTH0_CLIENT_ID;
-        this.Auth0Audience = process.env.AUTH0_AUDIENCE;
+        this.Auth0IssuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL || '';
+        this.Auth0ClientId = process.env.AUTH0_CLIENT_ID || '';
+        this.Auth0Audience = process.env.AUTH0_AUDIENCE || '';
 
         this.BaseUrl = `http://localhost:${this.Port}`;
 
