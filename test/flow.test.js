9af4c22a4a4917fcd1cf0daf3d997aa9312ac136
Vitor George <vitor.george@gmail.com>
2021-02-02 19:03:06 +0000

    Update flow test

--- test/flow.test.js
+++ test/flow.test.js
@@ -16,219 +16,171 @@ const test = require('tape');
 
 const WebSocket = require('ws');
 
-const usr = 'example-' + Math.floor(Math.random() * Math.floor(10 ^ 1000));
+const { Client, Pool } = require('pg');
+const Config = require('../services/api/lib/config');
 
-let flight;
-if (process.env.TEST !== 'compose') {
-    const { Flight } = require('./util');
-    flight = new Flight();
-    flight.takeoff(test);
-}
+main();
 
-const session = request.jar();
-let token, instance;
+async function main() {
 
-test('api running', (t) => {
-    request({
-        method: 'GET',
-        json: true,
-        url: API + '/api'
-    } , (err, res, body) => {
-        t.error(err, 'no error');
+    // Setup auth client to create user programmatically
+    const config = await Config.env();
+    const client = new Client({
+        connectionString: 'postgres://docker:docker@localhost:5433/gis'
+    });
+    const pool = new Pool({
+        connectionString: 'postgres://docker:docker@localhost:5433/gis'
+    });
 
-        t.equals(res.statusCode, 200);
+    let flight;
+    if (process.env.TEST !== 'compose') {
+        const { Flight } = require('./util');
+        flight = new Flight();
+        flight.takeoff(test);
+    } else {
+        // Serve should be running already, clean tables
+        await client.connect();
+        await client.query(`
+            TRUNCATE TABLE users;
+            TRUNCATE TABLE users_reset;
+            TRUNCATE TABLE models;
+            TRUNCATE TABLE instances;
+            TRUNCATE TABLE checkpoints;
+        `);
+        await client.end();
 
-        t.deepEquals(body, {
-            version: '1.0.0'
+    }
+
+    // Create a user in the database, generate a token to be
+    // used in the API
+    const auth = new (require('../services/api/lib/auth').Auth)(pool);
+    const authtoken = new (require('../services/api/lib/auth').AuthToken)(pool, config);
+    const testUser = {
+        username: 'example',
+        email: 'example@example.com',
+        password: 'password123'
+    };
+    await auth.register(testUser);
+    const user = await auth.login(testUser);
+    const { token } = await authtoken.generate(user, 'API Token');
+
+    let instance;
+
+    test('api running', (t) => {
+        request({
+            method: 'GET',
+            json: true,
+            url: API + '/api'
+        } , (err, res, body) => {
+            t.error(err, 'no error');
+
+            t.equals(res.statusCode, 200);
+
+            t.deepEquals(body, {
+                version: '1.0.0'
+            });
+
+            t.end();
         });
-
-        t.end();
     });
-});
 
-test('new user', (t) => {
-    request({
-        method: 'POST',
-        json: true,
-        url: API + '/api/user',
-        body: {
-            username: usr ,
-            email: `${usr}@example.com`,
-            password: 'password123'
-        }
-    } , (err, res, body) => {
-        t.error(err, 'no error');
+    test('new model', (t) => {
+        request({
+            method: 'POST',
+            json: true,
+            url: API + '/api/model',
+            body: {
+                name: 'NAIP Supervised',
+                active: true,
+                model_type: 'keras_example',
+                model_finetunelayer: -4,
+                model_numparams: 7790949,
+                model_inputshape: [240,240,4],
+                classes: [
+                    { name: 'Water', color: '#0000FF' },
+                    { name: 'Tree Canopy', color: '#008000' },
+                    { name: 'Field', color: '#80FF80' },
+                    { name: 'Built', color: '#806060' }
+                ],
+                meta: {}
+            },
+            headers: {
+                Authorization: `Bearer api.${token}`
+            }
+        } , (err, res, body) => {
+            t.error(err, 'no error');
 
-        t.equals(res.statusCode, 200, '200 status code');
+            t.equals(res.statusCode, 200, '200 status code');
 
-        t.deepEquals(body, {
-            status: 200,
-            message: 'User Created'
-        }, 'expected body');
+            t.deepEquals(Object.keys(body), [
+                'id',
+                'created'
+            ], 'expected props');
 
-        t.end();
+            t.ok(parseInt(body.id), 'id: <integer>');
+
+            delete body.created;
+            delete body.id;
+
+            t.deepEquals(body, {
+            }, 'expected body');
+
+            t.end();
+        });
     });
-});
 
-test('new session', (t) => {
-    request({
-        method: 'POST',
-        json: true,
-        url: API + '/api/login',
-        jar: session,
-        body: {
-            username: usr,
-            password: 'password123'
-        }
-    } , (err, res, body) => {
-        t.error(err, 'no error');
+    test('new instance', (t) => {
+        request({
+            method: 'POST',
+            json: true,
+            url: API + '/api/instance',
+            body: {
+                model_id: 1,
+                mosaic: 'naip.latest'
+            },
+            headers: {
+                Authorization: `Bearer api.${token}`
+            }
+        } , (err, res, body) => {
+            t.error(err, 'no error');
 
-        t.equals(res.statusCode, 200, '200 status code');
+            t.equals(res.statusCode, 200, '200 status code');
 
-        t.deepEquals(body, {
-            username: usr
-        }, 'expected body');
+            t.deepEquals(Object.keys(body), [
+                'id',
+                'created',
+                'model_id',
+                'token',
+                'mosaic'
+            ], 'expected props');
 
-        t.equals(res.headers['set-cookie'].length, 1, '1 cookie is set');
-        t.equals(res.headers['set-cookie'][0].split('=')[0], 'session', 'session cookie is set');
+            t.ok(parseInt(body.id), 'id: <integer>');
+            delete body.id,
+            delete body.created;
+            instance = body.token;
+            delete body.token;
 
-        t.end();
+            t.deepEquals(body, {
+                model_id: 1,
+                mosaic: 'naip.latest'
+            }, 'expected body');
+
+            t.end();
+        });
     });
-});
 
-test('new token', (t) => {
-    request({
-        method: 'POST',
-        json: true,
-        jar: session,
-        url: API + '/api/token',
-        body: {
-            name: 'Access Token'
-        }
-    } , (err, res, body) => {
-        t.error(err, 'no error');
+    test('gpu connection', (t) => {
+        const ws = new WebSocket(SOCKET + `?token=${instance}`);
 
-        t.equals(res.statusCode, 200, '200 status code');
-
-        t.deepEquals(Object.keys(body), [
-            'id',
-            'name',
-            'token',
-            'created'
-        ], 'expected props');
-
-        t.ok(parseInt(body.id), 'id: <integer>');
-
-        delete body.created;
-        delete body.id;
-
-        token = body.token;
-        delete body.token;
-
-        t.deepEquals(body, {
-            name: 'Access Token'
-        }, 'expected body');
-
-        t.end();
+        ws.on('open', () => {
+            t.ok('connection opened');
+            ws.close();
+            t.end();
+        });
     });
-});
 
-test('new model', (t) => {
-    request({
-        method: 'POST',
-        json: true,
-        url: API + '/api/model',
-        body: {
-            name: 'NAIP Supervised',
-            active: true,
-            model_type: 'keras_example',
-            model_finetunelayer: -4,
-            model_numparams: 7790949,
-            model_inputshape: [240,240,4],
-            classes: [
-                { name: 'Water', color: '#0000FF' },
-                { name: 'Tree Canopy', color: '#008000' },
-                { name: 'Field', color: '#80FF80' },
-                { name: 'Built', color: '#806060' }
-            ],
-            meta: {}
-        },
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    } , (err, res, body) => {
-        t.error(err, 'no error');
-
-        t.equals(res.statusCode, 200, '200 status code');
-
-        t.deepEquals(Object.keys(body), [
-            'id',
-            'created'
-        ], 'expected props');
-
-        t.ok(parseInt(body.id), 'id: <integer>');
-
-        delete body.created;
-        delete body.id;
-
-        t.deepEquals(body, {
-        }, 'expected body');
-
-        t.end();
-    });
-});
-
-test('new instance', (t) => {
-    request({
-        method: 'POST',
-        json: true,
-        url: API + '/api/instance',
-        body: {
-            model_id: 1,
-            mosaic: 'naip.latest'
-        },
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    } , (err, res, body) => {
-        t.error(err, 'no error');
-
-        t.equals(res.statusCode, 200, '200 status code');
-
-        t.deepEquals(Object.keys(body), [
-            'id',
-            'created',
-            'model_id',
-            'token',
-            'mosaic'
-        ], 'expected props');
-
-        t.ok(parseInt(body.id), 'id: <integer>');
-        delete body.id,
-        delete body.created;
-        instance = body.token;
-        delete body.token;
-
-        t.deepEquals(body, {
-            model_id: 1,
-            mosaic: 'naip.latest'
-        }, 'expected body');
-
-        t.end();
-    });
-});
-
-test('gpu connection', (t) => {
-    const ws = new WebSocket(SOCKET + `?token=${instance}`);
-
-    ws.on('open', () => {
-        t.ok('connection opened');
-        ws.close();
-        t.end();
-    });
-});
-
-if (process.env.TEST !== 'compose') {
-    flight.landing(test);
+    if (process.env.TEST !== 'compose') {
+        flight.landing(test);
+    }
 }
 
