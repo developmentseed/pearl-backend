9b8539490ea2c82a5d4a21de0279dee1a96b4a43
martham93 <marthamorrissey93@gmail.com>
2021-02-25 13:46:52 -0700

    switch starter model from UNET to FCN

--- services/gpu/web_tool/ModelSessionPyTorchExample.py
+++ services/gpu/web_tool/ModelSessionPyTorchExample.py
@@ -26,6 +26,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class FCN(nn.Module):
+
+    def __init__(self, num_input_channels, num_output_classes, num_filters=64):
+        super(FCN,self).__init__()
+
+        self.conv1 = nn.Conv2d(num_input_channels, num_filters, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(num_filters, num_filters,        kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(num_filters, num_filters,        kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(num_filters, num_filters,        kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(num_filters, num_filters,        kernel_size=3, stride=1, padding=1)
+        self.last =  nn.Conv2d(num_filters, num_output_classes, kernel_size=1, stride=1, padding=0)
+
+    def forward(self,inputs):
+        x = F.relu(self.conv1(inputs))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = self.last(x)
+        return x
 
 class TorchFineTuning(ModelSession):
 
@@ -53,10 +73,7 @@ class TorchFineTuning(ModelSession):
         # self.stride_y = self.input_size - self.down_weight_padding*2
 
         ### TODO
-        self.model = smp.Unet(
-            encoder_name='resnet18', encoder_depth=3, encoder_weights=None,
-            decoder_channels=(128, 64, 64), in_channels=4, classes=10 #this is hard-ocded need to fix
-            )
+        self.model = FCN(num_input_channels=4, num_output_classes=10, num_filters=64) #to-do fix that 10 is hardcoded
         self._init_model()
 
         for param in self.model.parameters():
