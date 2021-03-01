503df6a70e562e2a41ff7c76fae1c6f86a21ba17
Sajjad Anwar <sajjadkm@gmail.com>
2021-03-01 17:53:31 +0530

    gpu and websocket works. uncomment flow test

--- test/flow.test.js
+++ test/flow.test.js
@@ -20,204 +20,203 @@ const { Pool } = require('pg');
 const API = process.env.API || 'https://api.lulc.ds.io';
 const SOCKET = process.env.SOCKET || 'wss://socket.lulc.ds.io';
 
-// const { Client } = require('pg');
-// const drop = require('../services/api/test/drop');
+const { Client } = require('pg');
+const drop = require('../services/api/test/drop');
 
-// let token, instance;
-const instance = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0IjoiaW5zdCIsInUiOjIsImkiOjcsImlhdCI6MTYxNDI2NjQ2OSwiZXhwIjoxNjE0MzA5NjY5fQ.IzKob95gJY8NajvEjZOe9aloOMI-kXxHx1hKLH2WGHY';
+let token, instance;
 
 process.env.Postgres = process.env.Postgres || 'postgres://docker:docker@localhost:5433/gis';
 
-// test('pre-run', async (t) => {
-//     try {
-//         const config = await drop();
+test('pre-run', async (t) => {
+    try {
+        const config = await drop();
 
-//         const pool = new Pool({
-//             connectionString: config.Postgres
-//         });
+        const pool = new Pool({
+            connectionString: config.Postgres
+        });
 
-//         await pool.query(String(fs.readFileSync(path.resolve(__dirname, '../services/api/schema.sql'))));
+        await pool.query(String(fs.readFileSync(path.resolve(__dirname, '../services/api/schema.sql'))));
 
-//         const auth = new (require('../services/api/lib/auth').Auth)(pool);
-//         const authtoken = new (require('../services/api/lib/auth').AuthToken)(pool, config);
+        const auth = new (require('../services/api/lib/auth').Auth)(pool);
+        const authtoken = new (require('../services/api/lib/auth').AuthToken)(pool, config);
 
-//         const user = await auth.create({
-//             access: 'admin',
-//             username: 'example',
-//             email: 'example@example.com',
-//             auth0Id: 0
-//         });
+        const user = await auth.create({
+            access: 'admin',
+            username: 'example',
+            email: 'example@example.com',
+            auth0Id: 0
+        });
 
-//         token = (await authtoken.generate({
-//             type: 'auth0',
-//             uid: 1
-//         }, 'API Token')).token;
+        token = (await authtoken.generate({
+            type: 'auth0',
+            uid: 1
+        }, 'API Token')).token;
 
-//         pool.end();
-//     } catch (err) {
-//         t.error(err, 'no errors');
-//     }
+        pool.end();
+    } catch (err) {
+        t.error(err, 'no errors');
+    }
 
-//     t.end();
-// });
+    t.end();
+});
 
-// test('api running', (t) => {
-//     request({
-//         method: 'GET',
-//         json: true,
-//         url: API + '/api'
-//     }, (err, res, body) => {
-//         t.error(err, 'no error');
+test('api running', (t) => {
+    request({
+        method: 'GET',
+        json: true,
+        url: API + '/api'
+    }, (err, res, body) => {
+        t.error(err, 'no error');
 
-//         t.equals(res.statusCode, 200);
+        t.equals(res.statusCode, 200);
 
-//         t.deepEquals(body, {
-//             version: '1.0.0',
-//             limits: { live_inference: 1000, max_inference: 100000 }
-//         });
+        t.deepEquals(body, {
+            version: '1.0.0',
+            limits: { live_inference: 1000, max_inference: 100000 }
+        });
 
-//         t.end();
-//     });
-// });
+        t.end();
+    });
+});
 
-// test('new model', (t) => {
-//     request({
-//         method: 'POST',
-//         json: true,
-//         url: API + '/api/model',
-//         body: {
-//             name: 'NAIP Supervised',
-//             active: true,
-//             model_type: 'pytorch_example',
-//             model_inputshape: [256, 256, 4],
-//             model_zoom: 17,
-//             classes: [
-//                 { name: 'No Data', color: '#62a092' },
-//                 { name: 'Water', color: '#0000FF' },
-//                 { name: 'Emergent Wetlands', color: '#008000' },
-//                 { name: 'Tree Canopy', color: '#80FF80' },
-//                 { name: 'Shrubland', color: '#806060' },
-//                 { name: 'Low Vegetation', color: '#07c4c5' },
-//                 { name: 'Barren', color: '#027fdc' },
-//                 { name: 'Structure', color: '#f76f73' },
-//                 { name: 'Imprervious Surface', color: '#ffb703' },
-//                 { name: 'Imprevious Road', color: '#0218a2' }
-//             ],
-//             meta: {}
-//         },
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     }, (err, res, body) => {
-//         t.error(err, 'no error');
+test('new model', (t) => {
+    request({
+        method: 'POST',
+        json: true,
+        url: API + '/api/model',
+        body: {
+            name: 'NAIP Supervised',
+            active: true,
+            model_type: 'pytorch_example',
+            model_inputshape: [256, 256, 4],
+            model_zoom: 17,
+            classes: [
+                { name: 'No Data', color: '#62a092' },
+                { name: 'Water', color: '#0000FF' },
+                { name: 'Emergent Wetlands', color: '#008000' },
+                { name: 'Tree Canopy', color: '#80FF80' },
+                { name: 'Shrubland', color: '#806060' },
+                { name: 'Low Vegetation', color: '#07c4c5' },
+                { name: 'Barren', color: '#027fdc' },
+                { name: 'Structure', color: '#f76f73' },
+                { name: 'Imprervious Surface', color: '#ffb703' },
+                { name: 'Imprevious Road', color: '#0218a2' }
+            ],
+            meta: {}
+        },
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }, (err, res, body) => {
+        t.error(err, 'no error');
 
-//         t.equals(res.statusCode, 200, '200 status code');
+        t.equals(res.statusCode, 200, '200 status code');
 
-//         t.deepEquals(Object.keys(body), [
-//             'id', 'created', 'active', 'uid', 'name', 'model_type', 'model_inputshape', 'model_zoom', 'storage', 'classes', 'meta'
-//         ], 'expected props');
+        t.deepEquals(Object.keys(body), [
+            'id', 'created', 'active', 'uid', 'name', 'model_type', 'model_inputshape', 'model_zoom', 'storage', 'classes', 'meta'
+        ], 'expected props');
 
-//         t.ok(parseInt(body.id), 'id: <integer>');
+        t.ok(parseInt(body.id), 'id: <integer>');
 
-//         delete body.created;
-//         delete body.id;
+        delete body.created;
+        delete body.id;
 
-//         t.deepEquals(body, {
-//             active: true,
-//             uid: 1,
-//             name: 'NAIP Supervised',
-//             model_type: 'pytorch_example',
-//             model_inputshape: [256, 256, 4],
-//             model_zoom: 17,
-//             storage: null,
-//             classes: [
-//                 { name: 'No Data', color: '#62a092' },
-//                 { name: 'Water', color: '#0000FF' },
-//                 { name: 'Emergent Wetlands', color: '#008000' },
-//                 { name: 'Tree Canopy', color: '#80FF80' },
-//                 { name: 'Shrubland', color: '#806060' },
-//                 { name: 'Low Vegetation', color: '#07c4c5' },
-//                 { name: 'Barren', color: '#027fdc' },
-//                 { name: 'Structure', color: '#f76f73' },
-//                 { name: 'Imprervious Surface', color: '#ffb703' },
-//                 { name: 'Imprevious Road', color: '#0218a2' }
-//             ],
-//             meta: {}
-//         }, 'expected body');
+        t.deepEquals(body, {
+            active: true,
+            uid: 1,
+            name: 'NAIP Supervised',
+            model_type: 'pytorch_example',
+            model_inputshape: [256, 256, 4],
+            model_zoom: 17,
+            storage: null,
+            classes: [
+                { name: 'No Data', color: '#62a092' },
+                { name: 'Water', color: '#0000FF' },
+                { name: 'Emergent Wetlands', color: '#008000' },
+                { name: 'Tree Canopy', color: '#80FF80' },
+                { name: 'Shrubland', color: '#806060' },
+                { name: 'Low Vegetation', color: '#07c4c5' },
+                { name: 'Barren', color: '#027fdc' },
+                { name: 'Structure', color: '#f76f73' },
+                { name: 'Imprervious Surface', color: '#ffb703' },
+                { name: 'Imprevious Road', color: '#0218a2' }
+            ],
+            meta: {}
+        }, 'expected body');
 
-//         t.end();
-//     });
-// });
+        t.end();
+    });
+});
 
-// test('new project', (t) => {
-//     request({
-//         method: 'POST',
-//         json: true,
-//         url: API + '/api/project',
-//         body: {
-//             name: 'Test Project',
-//             model_id: 1,
-//             mosaic: 'naip.latest'
-//         },
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     }, (err, res, body) => {
-//         t.error(err, 'no error');
+test('new project', (t) => {
+    request({
+        method: 'POST',
+        json: true,
+        url: API + '/api/project',
+        body: {
+            name: 'Test Project',
+            model_id: 1,
+            mosaic: 'naip.latest'
+        },
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }, (err, res, body) => {
+        t.error(err, 'no error');
 
-//         t.equals(res.statusCode, 200, '200 status code');
+        t.equals(res.statusCode, 200, '200 status code');
 
-//         t.deepEquals(Object.keys(body).sort(), [
-//             'created', 'id', 'model_id', 'mosaic', 'name'
-//         ], 'expected props');
+        t.deepEquals(Object.keys(body).sort(), [
+            'created', 'id', 'model_id', 'mosaic', 'name'
+        ], 'expected props');
 
-//         t.ok(body.created, 'created: <date>');
+        t.ok(body.created, 'created: <date>');
 
-//         delete body.created;
+        delete body.created;
 
-//         t.deepEquals(body, {
-//             id: 1,
-//             name: 'Test Project',
-//             model_id: 1,
-//             mosaic: 'naip.latest'
-//         }, 'expected body');
+        t.deepEquals(body, {
+            id: 1,
+            name: 'Test Project',
+            model_id: 1,
+            mosaic: 'naip.latest'
+        }, 'expected body');
 
-//         t.end();
-//     });
-// });
+        t.end();
+    });
+});
 
-// test('new instance', (t) => {
-//     request({
-//         method: 'POST',
-//         json: true,
-//         url: API + '/api/project/1/instance',
-//         body: {},
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     }, (err, res, body) => {
-//         t.error(err, 'no error');
+test('new instance', (t) => {
+    request({
+        method: 'POST',
+        json: true,
+        url: API + '/api/project/1/instance',
+        body: {},
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }, (err, res, body) => {
+        t.error(err, 'no error');
 
-//         t.equals(res.statusCode, 200, '200 status code');
+        t.equals(res.statusCode, 200, '200 status code');
 
-//         t.deepEquals(Object.keys(body).sort(), [
-//             'created', 'id', 'token'
-//         ], 'expected props');
+        t.deepEquals(Object.keys(body).sort(), [
+            'created', 'id', 'token'
+        ], 'expected props');
 
-//         t.ok(parseInt(body.id), 'id: <integer>');
+        t.ok(parseInt(body.id), 'id: <integer>');
 
-//         delete body.id,
-//         delete body.created;
+        delete body.id,
+        delete body.created;
 
-//         instance = body.token;
+        instance = body.token;
 
-//         delete body.token;
+        delete body.token;
 
-//         t.deepEquals(body, {}, 'expected body');
+        t.deepEquals(body, {}, 'expected body');
 
-//         t.end();
-//     });
-// });
+        t.end();
+    });
+});
 
 test('gpu connection', (t) => {
     const ws = new WebSocket(SOCKET + `?token=${instance}`);
