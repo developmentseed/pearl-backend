ac7ae623b211aa16be3c22201dd8d64dfa27452a
ingalls <nick@ingalls.ca>
2021-03-01 08:55:34 -0700

    Add the ability to rename a checkpoint

--- services/api/doc/api_project.js
+++ services/api/doc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-01T15:30:45.156Z",
+    "time": "2021-03-01T15:46:39.152Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
