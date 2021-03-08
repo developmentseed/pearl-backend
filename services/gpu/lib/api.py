e535485dbdbfe95672e75e0a1faffcfe3f47c40c
ingalls <nick@ingalls.ca>
2021-03-08 08:26:45 -0700

    Fix path

--- services/gpu/lib/api.py
+++ services/gpu/lib/api.py
@@ -26,7 +26,7 @@ class API():
         self.url = url
         self.token = token
 
-        self.tmp_dir = '/tmp/gpu-api/'
+        self.tmp_dir = '/tmp/gpu-api'
 
         os.makedirs(self.tmp_dir, exist_ok=True)
         os.makedirs(self.tmp_dir + '/tiles', exist_ok=True)
