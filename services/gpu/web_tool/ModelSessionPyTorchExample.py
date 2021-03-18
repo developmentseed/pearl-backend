277bc9ddfc9ead8461a424336c2470094962c8b7
Sanjay B <sanjaybhangar@gmail.com>
2021-03-18 13:00:05 +0530

    actually resolve conflicts, sorry, VSCode weirdness

--- services/gpu/web_tool/ModelSessionPyTorchExample.py
+++ services/gpu/web_tool/ModelSessionPyTorchExample.py
@@ -77,17 +77,11 @@ class TorchFineTuning(ModelSession):
         self.classes = api.model['classes']
 
         self.model_fs = api.model_fs
-<<<<<<< HEAD
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-<<<<<<< HEAD
-=======
+        # self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
         print('# is cuda available?', torch.cuda.is_available())
-        print('# GPU or CPU?', self.device)
->>>>>>> [deploy] more logs
-=======
         self.device = torch.device("cuda")
-        print(self.device)
->>>>>>> caleb's gpu suggestions
+        print('# GPU or CPU?', self.device)
 
         # will need to figure out for re-training
         self.output_channels = len(self.classes)
