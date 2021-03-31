23cb55a0a2727e4ac70ade7b120fc7573e081c82
martham93 <marthamorrissey93@gmail.com>
2021-03-31 10:52:42 -0600

    remove unused utils

--- services/gpu/lib/utils.py
+++ services/gpu/lib/utils.py
@@ -245,53 +245,6 @@ class AtomicCounter:
             self.value += num
             return self.value
 
-def to_categorical(y, num_classes=None):
-    """Converts a class vector (integers) to binary class matrix.
-    E.g. for use with categorical_crossentropy.
-    # Arguments
-        y: class vector to be converted into a matrix
-            (integers from 0 to num_classes).
-        num_classes: total number of classes.
-    # Returns
-        A binary matrix representation of the input. The classes axis
-        is placed last.
-    """
-    y = np.array(y, dtype='int')
-    input_shape = y.shape
-    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
-        input_shape = tuple(input_shape[:-1])
-    y = y.ravel()
-    if not num_classes:
-        num_classes = np.max(y) + 1
-    n = y.shape[0]
-    categorical = np.zeros((n, num_classes), dtype=np.float32)
-    categorical[np.arange(n), y] = 1
-    output_shape = input_shape + (num_classes,)
-    categorical = np.reshape(categorical, output_shape)
-    return categorical
-
-
-def to_one_hot(im, class_num):
-    one_hot = np.zeros((class_num, im.shape[-2], im.shape[-1]), dtype=np.float32)
-    for class_id in range(class_num):
-        one_hot[class_id, :, :] = (im == class_id).astype(np.float32)
-    return one_hot
-
-def to_one_hot_batch(batch, class_num):
-    one_hot = np.zeros((batch.shape[0], class_num, batch.shape[-2], batch.shape[-1]), dtype=np.float32)
-    for class_id in range(class_num):
-        one_hot[:, class_id, :, :] = (batch == class_id).astype(np.float32)
-    return one_hot
-
-def nlcd_to_img(img):
-    return np.vectorize(NLCD_COLOR_MAP.__getitem__, signature='()->(n)')(img).astype(np.uint8)
-
-def get_shape_layer_by_name(shapes, layer_name):
-    for layer in shapes:
-        if layer["name"] == layer_name:
-            return layer
-    return None
-
 def get_random_string(length):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     return "".join([alphabet[np.random.randint(0, len(alphabet))] for i in range(length)])
