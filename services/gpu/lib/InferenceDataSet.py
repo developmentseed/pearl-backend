1ef3731ff0325d705217b4c2e8e0d36f89891bea
ingalls <nick@mapbox.com>
2021-04-26 13:45:11 -0600

    Fix get_tile

--- services/gpu/lib/InferenceDataSet.py
+++ services/gpu/lib/InferenceDataSet.py
@@ -20,9 +20,10 @@ class InferenceDataSet(Dataset):
         in_memraster = False
         while in_memraster is False:
             try:
-                in_memraster = self.modelsrv.api.get_tile(zxy.z, zxy.x, zxy.y)
+                in_memraster = self.api.get_tile(zxy.z, zxy.x, zxy.y)
             except:
                 print("InferenceDataSet ERROR", sys.exc_info()[0])
+
         tile = in_memraster.data
         tile = np.moveaxis(tile, -1, 0) #go from channels last to channels first (all MVP pytorch models will want the image tile to be (4, 256, 256))
         tile = tile / 255.0
