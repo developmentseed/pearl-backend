4da2fda300a7a1cb7fcb69ef691118588c0a699b
Sajjad Anwar <sajjadkm@gmail.com>
2021-07-14 12:12:33 +0530

    uncomment test

--- services/api/test/mosaic.test.js
+++ services/api/test/mosaic.test.js
@@ -34,20 +34,20 @@ test('GET /api/mosaic', (t) => {
     });
 });
 
-// test('GET /api/mosaic/naip.latest', (t) => {
-//     request({
-//         json: true,
-//         url: 'http://localhost:2000/api/mosaic/naip.latest',
-//         method: 'GET',
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     }, (err, res) => {
-//         t.error(err, 'no errors');
-//         t.equals(res.statusCode, 404, 'status: 404');
+test('GET /api/mosaic/naip.latest', (t) => {
+    request({
+        json: true,
+        url: 'http://localhost:2000/api/mosaic/naip.latest',
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }, (err, res) => {
+        t.error(err, 'no errors');
+        t.equals(res.statusCode, 404, 'status: 404');
 
-//         t.end();
-//     });
-// });
+        t.end();
+    });
+});
 
 flight.landing(test);
