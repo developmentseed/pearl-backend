812314a6d63c50923b2e01c05c0bece3c9ade3d8
Sajjad Anwar <sajjadkm@gmail.com>
2021-08-17 17:22:11 +0530

    add network peering. rename ip resource

--- deployment/terraform/resources/ip.tf
+++ deployment/terraform/resources/ip.tf
@@ -1,4 +1,4 @@
-resource "azurerm_public_ip" "lulcip" {
+resource "azurerm_public_ip" "lulc" {
   name                = "${local.prefix}PublicIP"
   resource_group_name = azurerm_resource_group.lulc.name
   location            = azurerm_resource_group.lulc.location
