9c9749a1953584fbe1bdc1f744c0610e81293cb3
Sajjad Anwar <sajjadkm@gmail.com>
2021-08-25 16:57:14 +0530

    fix kube auth

--- deployment/terraform/resources/helm-provider.tf
+++ deployment/terraform/resources/helm-provider.tf
@@ -1,8 +1,10 @@
 provider "helm" {
   kubernetes {
-    host = azurerm_kubernetes_cluster.lulc.kube_config.host
-    client_certificate = azurerm_kubernetes_cluster.lulc.kube_config.client_certificate
-    client_key = azurerm_kubernetes_cluster.lulc.kube_config.client_key
-    cluster_ca_certificate = azurerm_kubernetes_cluster.lulc.kube_config.cluster_ca_certificate
+    host = azurerm_kubernetes_cluster.lulc.kube_config.0.host
+    username = azurerm_kubernetes_cluster.lulc.kube_config.0.username
+    password = azurerm_kubernetes_cluster.lulc.kube_config.0.password
+    client_certificate = base64decode(azurerm_kubernetes_cluster.lulc.kube_config.0.client_certificate)
+    client_key = base64decode(azurerm_kubernetes_cluster.lulc.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.lulc.kube_config.0.cluster_ca_certificate)
   }
 }
