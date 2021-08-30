e7eb6088addf6dc19d9ea2f391fedb533d2b8efc
Sajjad Anwar <sajjadkm@gmail.com>
2021-08-30 19:17:08 +0530

    make acr owner

--- deployment/terraform/resources/acr.tf
+++ deployment/terraform/resources/acr.tf
@@ -6,6 +6,6 @@ data "azurerm_container_registry" "lulc" {
 # add the role to the identity the kubernetes cluster was assigned
 resource "azurerm_role_assignment" "attach_acr" {
   scope                = data.azurerm_container_registry.lulc.id
-  role_definition_name = "Contributor"
+  role_definition_name = "Owner"
   principal_id         = azurerm_kubernetes_cluster.lulc.kubelet_identity[0].object_id
 }
