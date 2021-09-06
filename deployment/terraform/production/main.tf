7f1b563c1b630c401f32f070cdd77ad390ebc523
Sajjad Anwar <sajjadkm@gmail.com>
2021-09-06 17:03:29 +0530

    [deploy] fix production main.tf. make staging deploy only on develop. test dev

--- deployment/terraform/production/main.tf
+++ deployment/terraform/production/main.tf
@@ -21,12 +21,14 @@ module "resources" {
   region               = "West Europe"
   aks_node_count       = 1
 
+  postgres_password = var.postgres_password
+  signing_secret = var.signing_secret
+
   gpu_count = 2
   placeholder_is_gpu = true
   placeholder_num_gpus = 2
 
-  # FIXME
-  # domain = "lulc-production.ds.io"
+  domain = "lulc-production.ds.io"
 }
 
 terraform {
