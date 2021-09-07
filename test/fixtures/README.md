3e434db088d2a4ca96e0f307d6f9fa4dd7c66851
ingalls <nick@ingalls.ca>
2021-09-07 07:51:13 -0600

    Improvments to Output class

---
+++ test/fixtures/README.md
@@ -0,0 +1,6 @@
+### Fixtures
+
+These fixtures represent inputs into both the scripted Flow tests
+as well as the interactive flow.test.js script. Output fixtures
+should NOT live here. Output fixtures should instead live in the `test/outputs`
+directory
--- test/prediction.test.js
+++ test/prediction.test.js
@@ -6,8 +6,12 @@ const test = require('tape');
 const API = process.env.API || 'http://localhost:2000';
 const SOCKET = process.env.SOCKET || 'ws://localhost:1999';
 
+// Top level tests are only designed to run with docker-compose
+process.env.Postgres = 'postgres://docker:docker@localhost:5433/gis'
+
 const { connect } = require('./init');
 const Worker = require('./worker');
+const Output = require('./output');
 
 state = connect(test, API);
 
@@ -17,254 +21,12 @@ const worker = new Worker(test, {
 
 worker.start();
 
-const flow = [{
-    message: 'model#checkpoint',
-    data: {
-        name: 'Seneca Rocks, WV',
-        id: 1
-    }
-},{
-    message: 'model#aoi',
-    data: {
-        id: 1,
-        live: true,
-        name: 'Seneca Rocks, WV',
-        checkpoint_id: 1,
-        bounds: [ -79.38446044921875, 38.82901019751964, -79.3707275390625, 38.83756825896614 ],
-        total: 20
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.83542884007304, -79.3817138671875, 38.83756825896614 ],
-        x: 36633,
-        y: 50169,
-        z: 17,
-        total: 20,
-        processed: 0
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 1
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 2
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 3
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.3817138671875, 38.83542884007304, -79.37896728515625, 38.83756825896614 ],
-        x: 36634,
-        y: 50169,
-        z: 17,
-        total: 20,
-        processed: 4
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 5
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 6
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 7
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 8
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 9
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 10
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 11
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 12
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 13
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 14
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 15
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 16
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 17
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 18
-    }
-},{
-    message: 'model#prediction',
-    data: {
-        aoi: 1,
-        bounds: [ -79.38446044921875, 38.833289356866885, -79.3817138671875, 38.83542884007304 ],
-        x: 36633,
-        y: 50170,
-        z: 17,
-        total: 20,
-        processed: 19
-    }
-},{
-    message: 'model#prediction#complete',
-    data: {
-        aoi: 1
-    }
-}].reverse();
-
 test('gpu connection', (t) => {
     state.connected = false;
     let close = false;
 
     const ws = new WebSocket(SOCKET + `?token=${state.instance.token}`);
+    const output = new Output(t, './outputs/prediction.test.json');
 
     sent = false;
     ws.on('message', (msg) => {
@@ -277,8 +39,7 @@ test('gpu connection', (t) => {
             t.ok('Sending model#prediction');
             ws.send(fs.readFileSync(path.resolve(__dirname, './fixtures/seneca-rocks/model#prediction.json')));
         } else {
-            if (msg.message === 'model#prediction') delete msg.data.image;
-            t.deepEquals(msg, flow.pop());
+            output.compare(msg);
         }
     });
 
