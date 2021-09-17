507940a78eee87ba0b029d9c83e9a958332eb2b3
martham93 <marthamorrissey93@gmail.com>
2021-09-17 12:30:47 -0600

    [deploy] inherit model

--- services/gpu/lib/ModelSessionDeepLabv3.py
+++ services/gpu/lib/ModelSessionDeepLabv3.py
@@ -20,106 +20,106 @@ sys.path.append("..")
 LOGGER = logging.getLogger("server")
 
 
-class DeepLabv3Plus(smp.base.SegmentationModel):
-    """DeepLabV3+ implementation from "Encoder-Decoder with Atrous Separable
-    Convolution for Semantic Image Segmentation"
-    Args:
-        encoder_name: Name of the classification model that will be used as an encoder (a.k.a backbone)
-            to extract features of different spatial resolution
-        encoder_depth: A number of stages used in encoder in range [3, 5]. Each stage generate features
-            two times smaller in spatial dimensions than previous one (e.g. for depth 0 we will have features
-            with shapes [(N, C, H, W),], for depth 1 - [(N, C, H, W), (N, C, H // 2, W // 2)] and so on).
-            Default is 5
-        encoder_weights: One of **None** (random initialization), **"imagenet"** (pre-training on ImageNet) and
-            other pretrained weights (see table with available weights for each encoder_name)
-        encoder_output_stride: Downsampling factor for last encoder features (see original paper for explanation)
-        decoder_atrous_rates: Dilation rates for ASPP module (should be a tuple of 3 integer values)
-        decoder_channels: A number of convolution filters in ASPP module. Default is 256
-        in_channels: A number of input channels for the model, default is 3 (RGB images)
-        classes: A number of classes for output mask (or you can think as a number of channels of output mask)
-        activation: An activation function to apply after the final convolution layer.
-            Available options are **"sigmoid"**, **"softmax"**, **"logsoftmax"**, **"tanh"**, **"identity"**, **callable** and **None**.
-            Default is **None**
-        upsampling: Final upsampling factor. Default is 4 to preserve input-output spatial shape identity
-        aux_params: Dictionary with parameters of the auxiliary output (classification head). Auxiliary output is build
-            on top of encoder if **aux_params** is not **None** (default). Supported params:
-                - classes (int): A number of classes
-                - pooling (str): One of "max", "avg". Default is "avg"
-                - dropout (float): Dropout factor in [0, 1)
-                - activation (str): An activation function to apply "sigmoid"/"softmax" (could be **None** to return logits)
-    Returns:
-    Reference:
-        https://arxiv.org/abs/1802.02611v3
-    """
+# class DeepLabv3Plus(smp.base.SegmentationModel):
+#     """DeepLabV3+ implementation from "Encoder-Decoder with Atrous Separable
+#     Convolution for Semantic Image Segmentation"
+#     Args:
+#         encoder_name: Name of the classification model that will be used as an encoder (a.k.a backbone)
+#             to extract features of different spatial resolution
+#         encoder_depth: A number of stages used in encoder in range [3, 5]. Each stage generate features
+#             two times smaller in spatial dimensions than previous one (e.g. for depth 0 we will have features
+#             with shapes [(N, C, H, W),], for depth 1 - [(N, C, H, W), (N, C, H // 2, W // 2)] and so on).
+#             Default is 5
+#         encoder_weights: One of **None** (random initialization), **"imagenet"** (pre-training on ImageNet) and
+#             other pretrained weights (see table with available weights for each encoder_name)
+#         encoder_output_stride: Downsampling factor for last encoder features (see original paper for explanation)
+#         decoder_atrous_rates: Dilation rates for ASPP module (should be a tuple of 3 integer values)
+#         decoder_channels: A number of convolution filters in ASPP module. Default is 256
+#         in_channels: A number of input channels for the model, default is 3 (RGB images)
+#         classes: A number of classes for output mask (or you can think as a number of channels of output mask)
+#         activation: An activation function to apply after the final convolution layer.
+#             Available options are **"sigmoid"**, **"softmax"**, **"logsoftmax"**, **"tanh"**, **"identity"**, **callable** and **None**.
+#             Default is **None**
+#         upsampling: Final upsampling factor. Default is 4 to preserve input-output spatial shape identity
+#         aux_params: Dictionary with parameters of the auxiliary output (classification head). Auxiliary output is build
+#             on top of encoder if **aux_params** is not **None** (default). Supported params:
+#                 - classes (int): A number of classes
+#                 - pooling (str): One of "max", "avg". Default is "avg"
+#                 - dropout (float): Dropout factor in [0, 1)
+#                 - activation (str): An activation function to apply "sigmoid"/"softmax" (could be **None** to return logits)
+#     Returns:
+#     Reference:
+#         https://arxiv.org/abs/1802.02611v3
+#     """
 
-    def __init__(
-        self,
-        encoder_name: str = "resnet34",
-        encoder_depth: int = 5,
-        encoder_weights: Optional[str] = "imagenet",
-        encoder_output_stride: int = 16,
-        decoder_channels: int = 256,
-        decoder_atrous_rates: tuple = (12, 24, 36),
-        in_channels: int = 3,
-        classes: int = 1,
-        activation: Optional[str] = None,
-        upsampling: int = 4,
-        aux_params: Optional[dict] = None,
-    ):
-        super().__init__()
+#     def __init__(
+#         self,
+#         encoder_name: str = "resnet34",
+#         encoder_depth: int = 5,
+#         encoder_weights: Optional[str] = "imagenet",
+#         encoder_output_stride: int = 16,
+#         decoder_channels: int = 256,
+#         decoder_atrous_rates: tuple = (12, 24, 36),
+#         in_channels: int = 3,
+#         classes: int = 1,
+#         activation: Optional[str] = None,
+#         upsampling: int = 4,
+#         aux_params: Optional[dict] = None,
+#     ):
+#         super().__init__()
 
-        if encoder_output_stride not in [8, 16]:
-            raise ValueError(
-                "Encoder output stride should be 8 or 16, got {}".format(
-                    encoder_output_stride
-                )
-            )
+#         if encoder_output_stride not in [8, 16]:
+#             raise ValueError(
+#                 "Encoder output stride should be 8 or 16, got {}".format(
+#                     encoder_output_stride
+#                 )
+#             )
 
-        self.encoder = smp.encoders.get_encoder(
-            encoder_name,
-            in_channels=in_channels,
-            depth=encoder_depth,
-            weights=encoder_weights,
-        )
+#         self.encoder = smp.encoders.get_encoder(
+#             encoder_name,
+#             in_channels=in_channels,
+#             depth=encoder_depth,
+#             weights=encoder_weights,
+#         )
 
-        self.decoder = smp.deeplabv3.decoder.DeepLabV3PlusDecoder(
-            encoder_channels=self.encoder.out_channels,
-            out_channels=decoder_channels,
-            atrous_rates=decoder_atrous_rates,
-            output_stride=encoder_output_stride,
-        )
+#         self.decoder = smp.deeplabv3.decoder.DeepLabV3PlusDecoder(
+#             encoder_channels=self.encoder.out_channels,
+#             out_channels=decoder_channels,
+#             atrous_rates=decoder_atrous_rates,
+#             output_stride=encoder_output_stride,
+#         )
 
-        self.segmentation_head = smp.base.SegmentationHead(
-            in_channels=self.decoder.out_channels,
-            out_channels=classes,
-            activation=activation,
-            kernel_size=1,
-            upsampling=upsampling,
-        )
+#         self.segmentation_head = smp.base.SegmentationHead(
+#             in_channels=self.decoder.out_channels,
+#             out_channels=classes,
+#             activation=activation,
+#             kernel_size=1,
+#             upsampling=upsampling,
+#         )
 
-        if aux_params is not None:
-            self.classification_head = smp.base.ClassificationHead(
-                in_channels=self.encoder.out_channels[-1], **aux_params
-            )
-        else:
-            self.classification_head = None
+#         if aux_params is not None:
+#             self.classification_head = smp.base.ClassificationHead(
+#                 in_channels=self.encoder.out_channels[-1], **aux_params
+#             )
+#         else:
+#             self.classification_head = None
 
-    def forward(self, x):
-        """Sequentially pass `x` trough model`s encoder, decoder and heads"""
-        features = self.encoder(x)
-        decoder_output = self.decoder(*features)
+# def forward(self, x):
+#     """Sequentially pass `x` trough model`s encoder, decoder and heads"""
+#     features = self.encoder(x)
+#     decoder_output = self.decoder(*features)
 
-        masks = self.segmentation_head(decoder_output)
+#     masks = self.segmentation_head(decoder_output)
 
-        if self.classification_head is not None:
-            labels = self.classification_head(features[-1])
-            return masks, labels
-        return masks
+#     if self.classification_head is not None:
+#         labels = self.classification_head(features[-1])
+#         return masks, labels
+#     return masks
 
-    def foward_features(self, x):
-        features = self.encoder(x)
-        decoder_output = self.decoder(*features)
-        return decoder_output
+# def foward_features(self, x):
+#     features = self.encoder(x)
+#     decoder_output = self.decoder(*features)
+#     return decoder_output
 
 
 class LoadDeepLabv3Plus(ModelSession):
@@ -156,7 +156,7 @@ class LoadDeepLabv3Plus(ModelSession):
         # will need to figure out for re-training
         self.output_channels = len(self.classes)
         self.output_features = 64
-        self.model = DeepLabv3Plus(
+        self.model = smp.DeepLabV3Plus(
             encoder_name="resnet18",
             encoder_weights=None,
             in_channels=4,
