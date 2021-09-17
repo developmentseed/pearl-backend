e869b044353b8f80c1160157413577f922e9bf78
martham93 <marthamorrissey93@gmail.com>
2021-09-16 22:52:06 -0600

    [deploy] debug

--- services/gpu/lib/ModelSessionDeepLabv3_j.py
+++ services/gpu/lib/ModelSessionDeepLabv3_j.py
@@ -372,9 +372,9 @@ class LoadDeepLabv3_j(ModelSession):
         output_preds = []
         data = tile.to(self.device)
         with torch.no_grad():
-            predictions = self.model(data)
+            predictions["out"] = self.model(data)
             predictions = (
-                F.softmax(predictions, dim=1).cpu().numpy()
+                F.softmax(predictions, dim=1).detach().numpy()
             )  # this is giving us the highest probability class per pixel
 
         for pred in predictions:
