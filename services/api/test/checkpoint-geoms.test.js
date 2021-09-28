e8772287f932e6dcf898118718a03ef790af24b1
ingalls <nick@ingalls.ca>
2021-09-28 10:45:52 -0600

    Fix rounding

--- services/api/test/checkpoint-geoms.test.js
+++ services/api/test/checkpoint-geoms.test.js
@@ -239,8 +239,8 @@ test('PATCH /api/project/1/checkpoint/1', async (t) => {
             storage: false,
             bookmarked: true,
             parent: null,
-            center: [-80.33203125, 43.032636960021215],
-            bounds: [-86.8359375, 34.88593094075317, -73.828125, 51.17934297928927],
+            center: [ -80.33203125, 43.0326369600212 ],
+            bounds: [ -86.8359375, 34.8859309407532, -73.828125, 51.1793429792893 ],
             classes: [
                 { name: 'Water', color: '#FF00FF' },
                 { name: 'Tree Canopy', color: '#008000' },
@@ -300,8 +300,8 @@ test('GET /api/project/1/checkpoint/1', async (t) => {
                 { name: 'Built', color: '#806060' }
             ],
             storage: false,
-            bounds: [-86.8359375, 34.88593094075317, -73.828125, 51.17934297928927],
-            center: [-80.33203125, 43.032636960021215],
+            center: [ -80.33203125, 43.0326369600212 ],
+            bounds: [ -86.8359375, 34.8859309407532, -73.828125, 51.1793429792893 ],
             retrain_geoms: [
                 { type: 'MultiPoint', coordinates: [[-86.8359375, 34.88593094075317]] },
                 { type: 'MultiPoint', coordinates: [[-86.8359375, 34.88593094075317], [-73.828125, 51.17934297928927]] },
@@ -470,8 +470,8 @@ test('GET /api/project/1/checkpoint/1/tiles - geometries', async (t) => {
             version: '1.0.0',
             schema: 'xyz',
             tiles: ['/project/1/checkpoint/1/tiles/{z}/{x}/{y}.mvt'],
-            bounds: [-86.8359375, 34.88593094075317, -73.828125, 51.17934297928927],
-            center: [-80.33203125, 43.032636960021215]
+            bounds: [ -86.8359375, 34.8859309407532, -73.828125, 51.1793429792893 ],
+            center: [ -80.33203125, 43.0326369600212 ]
         });
     } catch (err) {
         t.error(err, 'no errors');
