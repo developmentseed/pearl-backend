e56394fa3affb3f670d65394e90452939d039f14
ingalls <nick@ingalls.ca>
2021-09-29 10:33:21 -0600

    Modernize schema tests

--- services/api/test/schema.srv.test.js
+++ services/api/test/schema.srv.test.js
@@ -102,76 +102,17 @@ test('GET: api/schema?url=123', async (t) => {
 test('GET: api/schema?method=POST&url=/login', async (t) => {
     try {
         const res = await flight.request({
-            url: '/api/schema?method=POST&url=/login',
+            url: '/api/schema?method=GET&url=/schema',
             method: 'GET',
             json: true
         }, t);
 
-        t.equals(res.statusCode, 200, 'http: 200');
         t.deepEquals(res.body, {
-            body: {
-                type: 'object',
-                additionalProperties: false,
-                required: ['username', 'password'],
-                properties: {
-                    username: {
-                        type: 'string',
-                        description: 'username'
-                    },
-                    password: {
-                        type: 'string',
-                        description: 'password'
-                    }
-                }
-            },
-            query: null,
-            res: {
-                type: 'object',
-                required: ['uid', 'username', 'email', 'access', 'level', 'flags'],
-                additionalProperties: false,
-                properties: {
-                    token: { type: 'string' },
-                    uid: { type: 'integer' },
-                    username: { type: 'string' },
-                    email: { type: 'string' },
-                    access: { type: 'string', enum: ['user', 'disabled', 'admin'], description: 'The access level of a given user' },
-                    level: { type: 'string', enum: ['basic', 'backer', 'sponsor'], description: 'The level of donation of a given user' },
-                    flags: { type: 'object' }
-                }
-            }
+            query: require('../schema/req.query.ListSchema.json'),
+            res: require('../schema/res.ListSchema.json'),
+            body: null
         });
 
-    } catch (err) {
-        t.error(err, 'no error');
-    }
-
-    t.end();
-});
-
-test('POST: api/login', async (t) => {
-    try {
-        const res = await flight.request({
-            url: '/api/login',
-            method: 'POST',
-            json: true,
-            body: {
-                fake: 123,
-                username: 123
-            }
-        }, false);
-
-        t.equals(res.statusCode, 400, 'http: 400');
-        t.deepEquals(res.body, {
-            status: 400,
-            message: 'validation error',
-            messages: [{
-                message: 'should NOT have additional properties'
-            },{
-                message: 'should be string'
-            },{
-                message: 'should have required property \'password\''
-            }]
-        });
     } catch (err) {
         t.error(err, 'no error');
     }
