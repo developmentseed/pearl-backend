f0b682182ff799106e866f291f183fb3879becc1
ingalls <nick@ingalls.ca>
2021-10-01 11:27:40 -0600

    Update doics

--- services/api/doc/api_project.js
+++ services/api/doc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-09-30T15:26:24.630Z",
+    "time": "2021-10-01T17:27:33.529Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
