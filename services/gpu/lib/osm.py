cd967714d35212fcbc68dc381fce25680fb21e1b
ingalls <nick@ingalls.ca>
2021-10-07 12:50:19 -0600

    Sketch out tile fetching

--- services/gpu/lib/osm.py
+++ services/gpu/lib/osm.py
@@ -10,7 +10,7 @@ LOGGER = logging.getLogger("server")
 
 class OSM:
     def __init__(self):
-        self.url = ''
+        self.url = 'https://qa-tiles-server-dev.ds.io/services/z17'
 
         self.requests = requests.Session()
         adapter = HTTPAdapter(
@@ -25,11 +25,24 @@ class OSM:
         self.requests.mount('https://', adapter)
         self.requests.mount('http://', adapter)
 
-    def tile(self, x, y, z):
-        url = self.url + "/api"
+        self.tilejson = self.meta()
+
+    def meta(self):
+        LOGGER.info("ok - GET " + self.url)
+        r = self.requests.get(self.url)
+
+        r.raise_for_status()
+
+        LOGGER.info("ok - Received " + url)
+
+        return r.json();
+
+    def tile(self, x, y):
+        url = self.tilejson.get('tiles')[0].replace('{z}', str(17)).replace('{x}', str(x)).replace('{y}', str(y))
+
 
         LOGGER.info("ok - GET " + url)
-        r = self.requests.get(ur)
+        r = self.requests.get(url)
 
         r.raise_for_status()
 
