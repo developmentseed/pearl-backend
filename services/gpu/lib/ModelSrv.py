a25204788cb91cf3f1601ac6526080c573d87e6b
ingalls <nick@ingalls.ca>
2021-10-15 09:23:20 -0400

    Populate empty tagmaps

--- services/gpu/lib/ModelSrv.py
+++ services/gpu/lib/ModelSrv.py
@@ -521,6 +521,9 @@ class ModelSrv:
             osm.download(body.get('bounds'))
 
             for cls in body.get('classes'):
+                if cls.get('tagmap') is None:
+                    cls['tagmap'] = {}
+
                 cls['file'] = osm.extract(cls);
 
             done_processing(self)
