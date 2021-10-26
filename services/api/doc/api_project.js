00b341146d2c57c4fa75ae977fd40e70c2a9b059
Wille Marcel <wille.yyz@gmail.com>
2021-10-26 12:40:50 -0300

    Replace :pid by :projectid to fit the pattern

--- services/api/doc/api_project.js
+++ services/api/doc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-26T15:16:41.042Z",
+    "time": "2021-10-26T15:40:16.788Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
