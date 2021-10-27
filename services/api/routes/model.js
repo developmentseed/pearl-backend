24fdeb0d45380525ac63864949196c561e12b236
ingalls <nick@ingalls.ca>
2021-10-27 07:27:57 -0700

    Update Model

--- services/api/routes/model.js
+++ services/api/routes/model.js
@@ -219,6 +219,8 @@ async function router(schema, config) {
         try {
             const model = await Model.from(config.pool, req.params.modelid);
 
+            model.osmtag = await OSMTag.from(config.pool, req.params.modelid);
+
             return res.json(model.serialize());
         } catch (err) {
             return Err.respond(err, res);
