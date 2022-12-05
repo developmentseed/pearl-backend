65bfa87a89132b49d26c0664019bcc0ede452ee7
ingalls <nick@ingalls.ca>
2022-12-05 11:51:48 -0700

    Continue refining docker-compose stac service

--- services/stac/start.sh
+++ services/stac/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uvicorn api.app:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips='*'
+uvicorn api.app:app --host 0.0.0.0 --port 2002 --proxy-headers --forwarded-allow-ips='*'
