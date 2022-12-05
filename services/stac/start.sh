3b7328ad0df166aac90d7e3cebf43e0069472d4a
ingalls <nick@ingalls.ca>
2022-12-05 11:02:43 -0700

    Database migrations are performed by migration task

--- services/stac/start.sh
+++ services/stac/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-echo 'Run migrations if not already run'
-pypgstac migrate --dsn $DATABASE_URL
-
-psql $DATABASE_URL -f $(pwd)/mc_ricr_api/dbfuncs.sql
-
-uvicorn mc_ricr_api.app:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips='*'
+uvicorn api.app:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips='*'
