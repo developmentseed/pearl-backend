af5994b6d1a6071a4d779cf6f2dbc3a29faf48de
ingalls <nick@ingalls.ca>
2023-02-10 10:40:12 -0700

    Fix WS Docs

--- services/api/routes/websocket.js
+++ services/api/routes/websocket.js
@@ -92,9 +92,9 @@ export default async function router(schema) {
  */
 
 /**
- * @api {post} websocket Model#AOI
+ * @api {post} websocket Model#TimeFrame
  * @apiVersion 1.0.0
- * @apiName Model-AOI
+ * @apiName Model-TimeFrame
  * @apiGroup Websockets
  * @apiPermission user
  *
